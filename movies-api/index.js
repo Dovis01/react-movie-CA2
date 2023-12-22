c8b43ce8a6ec7a74e01164ab80a04a497479bec4
Shijin Zhang <1364226740@qq.com>
2023-12-22 19:22:28 +0800

    Add-swagger local deployment and finish the swagger API doc

--- movies-api/index.js
+++ movies-api/index.js
@@ -18,6 +18,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerDocument = YAML.load(path.join(__dirname, 'swaggerDoc.yml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
